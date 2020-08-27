2a4f3446b6bbd39fd188864e78bc0b81ff423c39
zusaaaa <obbyokamoto@gmail.com>
2020-08-27 16:27:14 +0900

    top_page-link_setting

--- db/migrate/20200827062141_add_details_to_titles.rb
+++ db/migrate/20200827062141_add_details_to_titles.rb
@@ -1,4 +1,4 @@
-class AddUsernameToUsers < ActiveRecord::Migration[5.2]
+class AddDetailsToTitles < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :username, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_034305) do
+ActiveRecord::Schema.define(version: 2020_08_27_071037) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "username"
+    t.text "post_content"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
