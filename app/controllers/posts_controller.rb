934b4212c719611a3452cb9286baabdcdbfefaec
zusaaaa <obbyokamoto@gmail.com>
2020-08-31 20:56:33 +0900

    post_details_impliment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,11 @@ class PostsController < ApplicationController
     redirect_to action: :index
   end
 
+  def show
+    @post = Post.find(params[:id])
+    @user = User.find(@post.user_id)
+  end
+
   private
 
   def post_params
